ced17e41f8c3afa9af11355b9fe22a7ce7aed1e4
HumanJHawkins <18685023+HumanJHawkins@users.noreply.github.com>
2020-02-22 15:56:39 -0800

    Test push after git issue.

--- js/mcArcs.js
+++ js/mcArcs.js
@@ -9,7 +9,7 @@ let gridSize;
 let arcArray;
 let radius;
 
-function newInstance(){
+function newInstance() {
     radius = document.getElementById("radius").value;
     // alert(getDistance(0,0,0,6));
     arcArray = getCircleArray(radius);
@@ -21,7 +21,7 @@ function handleDisplaySize() {
     //   best made based on actual pixel count, or aspect ratio.
     windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    if(windowWidth < windowHeight) {
+    if (windowWidth < windowHeight) {
         canvasSize = windowWidth * 0.95;
     } else {
         canvasSize = windowHeight * 0.95;
@@ -40,14 +40,14 @@ function handleDisplayRefresh() {
 }
 
 function drawCanvas(gridsize) {
-    theCanvas   = document.getElementById("arcCanvas");
-    theContext  = theCanvas.getContext("2d");
-    theCanvas.width     = canvasSize;
-    theCanvas.height    = canvasSize;
+    theCanvas = document.getElementById("arcCanvas");
+    theContext = theCanvas.getContext("2d");
+    theCanvas.width = canvasSize;
+    theCanvas.height = canvasSize;
     theCanvas.style.background = "#FBFBFF";
     theContext.restore();
     theContext.clearRect(0, 0, theCanvas.width, theCanvas.height);
-    theContext.translate(theCanvas.width/2, theCanvas.height/2);
+    theContext.translate(theCanvas.width / 2, theCanvas.height / 2);
     theContext.lineWidth = "1";
 }
 
@@ -59,11 +59,11 @@ function drawGrid() {
     // theContext.moveTo(0, -gridExtent);
     // theContext.lineTo( 0, gridExtent);
 
-    for(let i = gridSize / 2; i <= gridExtent; i += gridSize) {
+    for (let i = gridSize / 2; i <= gridExtent; i += gridSize) {
         theContext.moveTo(-gridExtent, i);
-        theContext.lineTo( gridExtent, i);
+        theContext.lineTo(gridExtent, i);
         theContext.moveTo(-gridExtent, -i);
-        theContext.lineTo( gridExtent, -i);
+        theContext.lineTo(gridExtent, -i);
         theContext.moveTo(i, -gridExtent);
         theContext.lineTo(i, gridExtent);
         theContext.moveTo(-i, -gridExtent);
@@ -85,41 +85,41 @@ function drawGridArc(circleArray, startDegree, endDegree) {
     // Light circle line
     theContext.strokeStyle = "red";
     theContext.beginPath();
-    theContext.arc(0, 0, radius*gridSize, 0, 2 * Math.PI);
+    theContext.arc(0, 0, radius * gridSize, 0, 2 * Math.PI);
     theContext.stroke();
 
-    if(circleArray) {
-        for(let i = 0; i < circleArray.length; i++) {
+    if (circleArray) {
+        for (let i = 0; i < circleArray.length; i++) {
             squareAt(circleArray[i]);
         }
-        squareAt([0,0], "red");
+        squareAt([0, 0], "red");
     }
 }
 
 function squareAt(p, color) {
     color = color || "black";
 
-    let sizeAdjust = gridSize/2;
+    let sizeAdjust = gridSize / 2;
     p[0] *= gridSize;
     p[1] *= -gridSize;
 
     theContext.strokeStyle = color;
     theContext.beginPath();
-    theContext.moveTo(p[0]-sizeAdjust, p[1]-sizeAdjust);
-    theContext.lineTo(p[0]-sizeAdjust, p[1]+sizeAdjust);
-    theContext.lineTo(p[0]+sizeAdjust, p[1]+sizeAdjust);
-    theContext.lineTo(p[0]+sizeAdjust, p[1]-sizeAdjust);
-    theContext.lineTo(p[0]-sizeAdjust, p[1]-sizeAdjust);
+    theContext.moveTo(p[0] - sizeAdjust, p[1] - sizeAdjust);
+    theContext.lineTo(p[0] - sizeAdjust, p[1] + sizeAdjust);
+    theContext.lineTo(p[0] + sizeAdjust, p[1] + sizeAdjust);
+    theContext.lineTo(p[0] + sizeAdjust, p[1] - sizeAdjust);
+    theContext.lineTo(p[0] - sizeAdjust, p[1] - sizeAdjust);
     theContext.stroke();
 }
 
-function getCircleArray(radius){
+function getCircleArray(radius) {
     let currentX = 0;
     let currentY = radius;
 
     // The first point (element 0) of our 1/8th circle arc is is always (0, radius).
     let circleArray = [currentX, currentY];
-    let endPoint = rotatePointInt([0,0], circleArray,45);
+    let endPoint = rotatePointInt([0, 0], circleArray, 45);
 
 
     // The last point of our 1/8th circle arc is the closest integer point to a 45 degree rotation of the first point.
@@ -129,9 +129,9 @@ function getCircleArray(radius){
 
     // For each x-value through 1/8th of the circle, the y-value will be either the same, or one less than the previous
     //   point's y-value. Between these two, use the y-value that is closest to the radius of the circle.
-    while(currentX <= endPoint[0]){
-        if(Math.abs(radius - getDistance(0,0, currentX,currentY - 1))
-            < Math.abs(radius - getDistance(0,0, currentX, currentY))){
+    while (currentX <= endPoint[0]) {
+        if (Math.abs(radius - getDistance(0, 0, currentX, currentY - 1))
+            < Math.abs(radius - getDistance(0, 0, currentX, currentY))) {
             currentY--;
         }
 
@@ -145,21 +145,21 @@ function getCircleArray(radius){
     // Create the rest of the circle from our 1/8th circle arc-segment.
     // Keep all points in clockwise-order to allow easy cutting of arc-segments from the array.
     let lastElement = circleArray.length - 1;
-    for(let i = 0; i < lastElement; i++) {  // 1/8 --> 1/4: Skip first element, to avoid duplicates on next copy.
-        circleArray[lastElement+1+i] = [circleArray[lastElement-i][1], circleArray[lastElement-i][0]];
+    for (let i = 0; i < lastElement; i++) {  // 1/8 --> 1/4: Skip first element, to avoid duplicates on next copy.
+        circleArray[lastElement + 1 + i] = [circleArray[lastElement - i][1], circleArray[lastElement - i][0]];
     }
 
     // Some squares at 45 degrees are unnecessary because the squares on either side connect at a point. Remove these.
-    if(circleArray[lastElement][1] === circleArray[lastElement-1][1]
-        && circleArray[lastElement][0] === circleArray[lastElement+1][0]) {
+    if (circleArray[lastElement][1] === circleArray[lastElement - 1][1]
+        && circleArray[lastElement][0] === circleArray[lastElement + 1][0]) {
         circleArray.splice(lastElement, 2);
     }
 
     let segmentLength = circleArray.length;
-    for(let i = 0; i < segmentLength; i++) {
-        circleArray[segmentLength+i] = rotatePointInt([0,0], circleArray[i], 90);
-        circleArray[segmentLength*2+i] = rotatePointInt([0,0], circleArray[i], 180);
-        circleArray[segmentLength*3+i] = rotatePointInt([0,0], circleArray[i], 270);
+    for (let i = 0; i < segmentLength; i++) {
+        circleArray[segmentLength + i] = rotatePointInt([0, 0], circleArray[i], 90);
+        circleArray[segmentLength * 2 + i] = rotatePointInt([0, 0], circleArray[i], 180);
+        circleArray[segmentLength * 3 + i] = rotatePointInt([0, 0], circleArray[i], 270);
     }
     return circleArray;
 }
@@ -174,6 +174,7 @@ function rotatePoint(c, p, angle) {
         (cos * (p[1] - c[1])) - (sin * (p[0] - c[0])) + c[1]
     ];
 }
+
 function rotatePointInt(c, p, angle) {
     let n = rotatePoint(c, p, angle);
     return [
@@ -183,5 +184,5 @@ function rotatePointInt(c, p, angle) {
 }
 
 function getDistance(x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
